454cd1ae9ba71d4c896cea48ed6d66b0ee3369d2
blueOcaen <937585067@qq.com>
2019-07-22 17:15:41 +0800

    第一次提交

---
+++ src/main/java/com/education/BesteducationiApplication.java
@@ -0,0 +1,13 @@
+package com.education;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BesteducationiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BesteducationiApplication.class, args);
+	}
+
+}
